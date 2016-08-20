11ca149497f2a48c375e4354a55f8b3de6f6d710
Kevin Zhang <whitehat2k9@gmail.com>
2016-08-19 22:30:18 -0400

    Single-file and multi-file modes tested and working

--- dottorrent.py
+++ dottorrent.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python3
 
+# MIT License
+
+# Copyright (c) 2016 Kevin Zhang
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from collections import OrderedDict
+from datetime import datetime
 from hashlib import sha1, md5
 import math
 import os
@@ -7,6 +32,12 @@ import sys
 from urllib.parse import urlparse
 
 from bencoder import bencode
+
+from version import __version__
+
+DEFAULT_CREATOR = "dottorrent/{} (https://github.com/kz26/dottorrent)".format(
+    __version__)
+
 
 MIN_PIECE_SIZE = 2 ** 14
 MAX_PIECE_SIZE = 2 ** 22
@@ -17,8 +48,19 @@ class Torrent(object):
     def __init__(self, path, trackers=None, http_seeds=None,
                  piece_size=None, private=False, creation_date=None,
                  comment=None, created_by=None):
+        """
+        path: path to a file or directory from which to create the torrent
+        trackers: list/iterable of tracker URLs
+        http_seeds: list/iterable of HTTP seed URLs
+        piece_size: Piece size in bytes. Must be >= 16 KB and a power of 2.
+        private: The private flag. If True, DHT/PEX will be disabled.
+        creation_date: A datetime object. If None, uses the current date/time.
+        comment: An optional comment string for the torrent.
+        created_by: name/version of the program used to create the .torrent.
+        If None, defaults to the value of DEFAULT_CREATOR.
+        """
 
-        self.path = path
+        self.path = os.path.normpath(path)
         self.trackers = trackers
         self.http_seeds = http_seeds
         self.piece_size = piece_size
@@ -80,8 +122,14 @@ class Torrent(object):
             self._piece_size = None
 
     def generate(self, include_md5=False):
+        """
+        Computes and stores piece data.
+        include_md5: If True, also computes and stores MD5 hashes for each file.
+        """
         self._files = []
-        if os.path.isfile(self.path):
+        self._include_md5 = include_md5
+        self._single_file = os.path.isfile(self.path)
+        if self._single_file:
             self._files.append((self.path, os.path.getsize(self.path)))
         else:
             for x in os.walk(self.path):
@@ -120,7 +168,58 @@ class Torrent(object):
                         md5_hasher.update(chunk)
                 if include_md5:
                     fe[2]['md5sum'] = md5_hasher.hexdigest()
+                f.close()
                 i += 1
+            # Add pieces from any remaining data
+            while len(buf):
+                piece = buf[:self.piece_size]
+                self._pieces += sha1(piece).digest()
+                del buf[:self.piece_size]
+
+        self._generated = True
 
     def save(self):
-        pass
+        """
+        Generates the bencoded torrent data.
+        """
+        if getattr(self, '_generated', False):
+            data = OrderedDict()
+            if len(self.trackers) == 1:
+                data['announce'] = self.trackers[0]
+            elif len(self.trackers) > 1:
+                data['announce-list'] = [[x] for x in self.trackers]
+            if self.comment:
+                data['comment'] = self.comment
+            if self.created_by:
+                data['created by'] = self.created_by
+            else:
+                data['created by'] = DEFAULT_CREATOR
+            if self.creation_date:
+                data['creation date'] = int(self.creation_date.timestamp())
+            else:
+                data['creation date'] = int(datetime.now().timestamp())
+            if self.http_seeds:
+                data['httpseeds'] = self.http_seeds
+            data['info'] = OrderedDict()
+            if self._single_file:
+                data['info']['length'] = self._files[0][1]
+                if self._include_md5:
+                    data['info']['md5sum'] = self._files[0][2]['md5sum']
+                data['info']['name'] = self._files[0][0].split(os.sep)[-1]
+            else:
+                data['info']['files'] = []
+                for x in self._files:
+                    fx = OrderedDict()
+                    fx['length'] = x[1]
+                    if self._include_md5:
+                        fx['md5sum'] = x[2]['md5sum']
+                    fx['path'] = x[0].replace(self.path, '')[1:].split(os.sep)
+                    data['info']['files'].append(fx)
+                data['info']['name'] = self.path.split(os.sep)[-1]
+            data['info']['pieces'] = bytes(self._pieces)
+            data['info']['piece length'] = self.piece_size
+            data['info']['private'] = int(self.private)
+            return bencode(data)
+        else:
+            raise Exception(
+                "Torrent not generated - call generate() before save()")
